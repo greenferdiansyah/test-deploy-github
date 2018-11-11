ca1c611411a5ca2efa790e679c016b5a42296007
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-11 09:16:06 +0000

    Deploy greenferdiansyah/test-deploy-github to github.com/greenferdiansyah/test-deploy-github.git:gh-pages

---
+++ precache-manifest.45e071bf93ab2a1da3185004acf16630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0af6415a7da17e2ef119",
+    "url": "/test-deploy-github/static/css/main.9d8f10e1.chunk.css"
+  },
+  {
+    "revision": "0af6415a7da17e2ef119",
+    "url": "/test-deploy-github/static/js/main.0af6415a.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/test-deploy-github/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "b688e05e6ed3a88ff1f0",
+    "url": "/test-deploy-github/static/js/runtime~main.b688e05e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy-github/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aaec4b785ee033cf36640367f52995ea",
+    "url": "/test-deploy-github/index.html"
+  }
+];
